3060495fff316ccd3a413620ce412184da289bdd
liujingjingg <949694044@qq.com>
2019-11-14 09:10:46 +0800

    zhuba

---
+++ precache-manifest.a7c290071d9dfc177dcdd943430cb498.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d927375d514fece7dac0c84b5561207d",
+    "url": "index.html"
+  },
+  {
+    "revision": "052c2d4ab2d2f4a9063e",
+    "url": "static/css/2.7cd62eb9.chunk.css"
+  },
+  {
+    "revision": "269980bbbd898fd44fdf",
+    "url": "static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "052c2d4ab2d2f4a9063e",
+    "url": "static/js/2.08c34681.chunk.js"
+  },
+  {
+    "revision": "269980bbbd898fd44fdf",
+    "url": "static/js/main.1b4519f7.chunk.js"
+  },
+  {
+    "revision": "067790caabdba6a1564e",
+    "url": "static/js/runtime-main.d10b7575.js"
+  },
+  {
+    "revision": "c5ed83639b4b2f50a307fd149f5cef31",
+    "url": "static/media/liantu2.c5ed8363.png"
+  },
+  {
+    "revision": "052419f0b8858d30815e35d50fd0934f",
+    "url": "static/media/liantu3.052419f0.png"
+  },
+  {
+    "revision": "e7dc1252219b6a35982217fe5afb136d",
+    "url": "static/media/liantu4.e7dc1252.png"
+  },
+  {
+    "revision": "b3965d407e8d57edd28cf504348f41a5",
+    "url": "static/media/linggan1.b3965d40.png"
+  },
+  {
+    "revision": "ac9cf52be01a421ae4d8b5854246d39d",
+    "url": "static/media/linggan2.ac9cf52b.png"
+  },
+  {
+    "revision": "89e1412fd5e61016f9deca961496a6d8",
+    "url": "static/media/lingtu1.89e1412f.png"
+  },
+  {
+    "revision": "fb28c3626b519c71ffd18bb7672fb33d",
+    "url": "static/media/shouye1.fb28c362.jpg"
+  },
+  {
+    "revision": "8bc4856ae322118d834557430db1bc17",
+    "url": "static/media/shouye2.8bc4856a.jpg"
+  },
+  {
+    "revision": "d9cfe5bec466795bb0376b624c5693c1",
+    "url": "static/media/shouye3.d9cfe5be.jpg"
+  },
+  {
+    "revision": "a74a333611eec1388832530af531b096",
+    "url": "static/media/shouye4.a74a3336.jpg"
+  },
+  {
+    "revision": "4ae06836c877edd6ce37da4f76b98240",
+    "url": "static/media/shouye5.4ae06836.jpg"
+  },
+  {
+    "revision": "cfb1feab0ac5abd1a154e5bdd8b5d827",
+    "url": "static/media/store1.cfb1feab.png"
+  },
+  {
+    "revision": "ac979fa228505731c14e32d05ed67b5e",
+    "url": "static/media/store2.ac979fa2.png"
+  },
+  {
+    "revision": "4e293ebfddbf8fbd4830e542f7b9116f",
+    "url": "static/media/store3.4e293ebf.png"
+  },
+  {
+    "revision": "5d3aca47a7fe976d51821c09dced435f",
+    "url": "static/media/store4.5d3aca47.png"
+  },
+  {
+    "revision": "fd45b29fa1614e1c2966ea5a765bed53",
+    "url": "static/media/wode1.fd45b29f.png"
+  }
+]);
